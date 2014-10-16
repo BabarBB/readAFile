d871efa6a0781b4b6521cf965731ad63ddb9d369
Blayid <blayid.ben.belkacem@depinfonancy.net>
2014-10-16 21:17:52 +0200

    lire dans un fichier
    prends une adresse de fichier .txt en entrée et renvoi une string du
    contenu du fichier

---
+++ readAFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readAFile
+{
+    class Program
+    {
+        public static string read_from_file(string adressOfFile) //ne pas oublier les doubles \\ pour l'adresse du fichier exemple : "C:\\Users\\blayid\\Documents\\Mines\\Info\\textfileIceSL.txt"
+        {
+            string text = System.IO.File.ReadAllText(adressOfFile);
+            return text;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
